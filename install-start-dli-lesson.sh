6e0301a52fcbab9403ee35208235c639e29bb875
jetbot <jetbot@nano-4gb-jp45.localdomain>
2023-03-13 20:57:47 -0700

    add start service

--- install-start-dli-lesson.sh
+++ install-start-dli-lesson.sh
@@ -10,3 +10,5 @@ sudo cp start-dli-lesson.service /etc/systemd/system/
 sudo systemctl daemon-reload
 
 sudo systemctl enable start-dli-lesson.service
+
+sudo systemctl start start-dli-lesson.service
